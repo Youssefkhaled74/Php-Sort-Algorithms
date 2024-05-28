5d2ec2032e0023579d52f074015200047464f13f
Youssefkhaled74 <121617652+Youssefkhaled74@users.noreply.github.com>
2024-05-28 16:06:44 +0300

    Create algorithms.php

---
+++ algorithms.php
@@ -0,0 +1,49 @@
+function bubbleSort($arr) {
+    $n = count($arr);
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+//-----------------------------------------------------------
+function selectionSort($arr) {
+    $n = count($arr);
+    for ($i = 0; $i < $n - 1; $i++) {
+        $minIndex = $i;
+        for ($j = $i + 1; $j < $n; $j++) {
+            if ($arr[$j] < $arr[$minIndex]) {
+                $minIndex = $j;
+            }
+        }
+        if ($minIndex != $i) {
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$minIndex];
+            $arr[$minIndex] = $temp;
+        }
+    }
+    return $arr;
+}
+//------------------------------------------------------------
+function quickSort($arr) {
+    $n = count($arr);
+    if ($n <= 1) {
+        return $arr;
+    }
+    $pivot = $arr[0];
+    $left = $right = array();
+    for ($i = 1; $i < $n; $i++) {
+        if ($arr[$i] < $pivot) {
+            $left[] = $arr[$i];
+        } else {
+            $right[] = $arr[$i];
+        }
+    }
+    return array_merge(quickSort($left), array($pivot), quickSort($right));
+}
+//-----------------------------------------------------------
